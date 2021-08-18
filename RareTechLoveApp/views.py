0b8e448583f8cac8784f1496302240f0e9fc0494
新森俊 <shinmoritakashi@shintaka.local>
2021-08-18 20:36:02 +0900

    login機能コミット

--- RareTechLoveApp/views.py
+++ RareTechLoveApp/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def topview(request):
+    return render(request, 'top.html', {'hello': 'Hello'})
+
+def signupview(request):
+    if request.method == 'POST':
+        username_data = request.POST.get('username_data')
+        password_data = request.POST.get('password_data')
+        try:
+            user = User.objects.create_user(username_data, '', password_data)
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+    else:
+        return render(request, 'signup.html', {})
+    return render(request, 'signup.html', {})
+
+def loginview(request):
+    if request.method == 'POST':
+        username_data = request.POST.get('username_data')
+        password_data = request.POST.get('password_data')
+        user = authenticate(request, username=username_data, password=password_data)
+        if user is not None:
+            login(request, user)
+            return redirect('top_page')
+        else: 
+            return redirect('login')
+    return render(request, 'login.html')
+
+def logoutview(request):
+    logout(request)
+    return redirect('login')
