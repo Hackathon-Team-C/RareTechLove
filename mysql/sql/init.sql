f92770069f7d18a7aac3b4d48cf03816d6c9ff0c
Junichi Nishi <github@purewest.dip.jp>
2021-09-11 03:25:35 +0900

    マイグレーション準備完了

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,45 +1,8 @@
-GRANT ALL PRIVILEGES ON db.* TO 'user'@'%';
+DROP TABLE IF EXISTS sample_table;
 
-FLUSH PRIVILEGES;
+CREATE TABLE sample_table (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name TEXT NOT NULL
+) charset=utf8;
 
-DROP DATABASE IF EXISTS raretechlovedb;
-CREATE DATABASE raretechlovedb;
-USE raretechlovedb;
-
-DROP TABLE IF EXISTS user_mst;
-CREATE TABLE user_mst
-(
-  user_cd INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_name VARCHAR(255) NOT NULL,
-  pw VARCHAR(255) NOT NULL,
-  slack_name VARCHAR(255) NOT NULL,
-);
-
-DROP TABLE IF EXISTS article_mst;
-CREATE TABLE article_mst
-(
-  artile_cd INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  artile_url VARCHAR(255) NOT NULL,
-  category VARCHAR(255) NOT NULL,
-  artile_rank VARCHAR(255) NOT NULL,
-  artile_title VARCHAR(21844) NOT NULL
-);
-
-DROP TABLE IF EXISTS q_count_tbl;
-CREATE TABLE q_count_tbl
-(
-  q_cd INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  q_cnt INT NULL,
-  a_cnt INT NULL,
-);
-
-DROP TABLE IF EXISTS question_tbl;
-CREATE TABLE question_tbl
-(
-  ts_cd INT NOT NULL AUTO_INCREMENT,
-  artile_cd INT NOT NULL,
-  user_cd INT NOT NULL,
-  quetion_thred VARCHAR(21844) NOT NULL,
-  ts timestamp DEFAULT NOT NULL,
-  qa_dist BOOLEAN NOT NULL,
-);
+INSERT INTO sample_table (name) VALUES ("太郎"),("花子"),("令和");
